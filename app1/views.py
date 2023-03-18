6baf01ad094cd2d1cdebf50338f5dbd87df16207
MachireddyManasa <machireddymanasa03@gmail.com>
2023-03-18 14:06:56 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def dhoni(request):
+    return render(request,'dhoni.html')
+
+
+def kohli(request):
+    return render(request,'kohli.html')
---
+++ app2/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def rohit(request):
+    return render(request,'rohit.html')
+
+
+
+def jadeja(request):
+    return render(request,'jadeja.html')
